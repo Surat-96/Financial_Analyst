a6bd30144bb483aee8bac0765df7e345aff54854
Surat Banerjee <67864248+Surat-96@users.noreply.github.com>
2025-09-15 17:47:16 +0530

    Add files via upload

---
+++ simpleagent.py
@@ -0,0 +1,22 @@
+from agno.agent import Agent
+from agno.models.openai import OpenAIChat
+from agno.models.groq import Groq
+from agno.tools.duckduckgo import DuckDuckGoTools
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+#os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
+os.environ["GROQ_API_KEY"]=os.getenv("GROQ_API_KEY")
+os.environ["GOOGLE_API_KEY"]=os.getenv("GOOGLE_API_KEY")
+
+
+agent=Agent(
+    model=Groq(id="qwen/qwen3-32b"),
+    description="You are an assistant please reply based ont he question",
+    tools=[DuckDuckGoTools()],
+    markdown=True
+)
+
+agent.print_response("Who won the India vs Newzealand finals in CT 2025")
